221d6994d72a1784db59dddbacc01874749cc90a
daotrongvui <daotrongvui070300@gmail.com>
2024-01-29 17:30:44 +0700

    [#29] [CRUD] Update course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 const handlebars = require("express-handlebars");
 
 const route = require("./routes");
@@ -21,6 +22,8 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride("_method"));
+
 // HTTP logger
 // app.use(morgan("combined"));
 
@@ -29,10 +32,13 @@ app.engine(
   "hbs",
   handlebars({
     extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources","views"));
+app.set("views", path.join(__dirname, "resources", "views"));
 
 // Route init
 
