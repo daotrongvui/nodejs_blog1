4a3714d4d90610cc03661e822e468eae62615a07
daotrongvui <daotrongvui070300@gmail.com>
2024-01-29 09:50:28 +0700

    [#25] Install JSON viewer extension

--- src/index.js
+++ src/index.js
@@ -32,12 +32,12 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
+app.set("views", path.join(__dirname, "resources","views"));
 
 // Route init
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
